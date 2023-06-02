2d66deafcebcb02aaf44cadc34d1ecd8c3492e4f
Divashree Agarwal <93401095+27Divashree@users.noreply.github.com>
2023-06-02 19:26:51 +0530

    Add files via upload

---
+++ create_moves.py
@@ -0,0 +1,39 @@
+import cv2
+
+
+def create_move(video_name):
+    cap = cv2.VideoCapture(0)
+
+    # Get the Default resolutions
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    fps = 24
+
+    # Define the codec and filename.
+    out = cv2.VideoWriter(
+        "dance_videos/{}.mp4".format(video_name),
+        cv2.VideoWriter_fourcc("M", "J", "P", "G"),
+        fps,
+        (frame_width, frame_height),
+    )
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret == True:
+            # write the  frame
+            out.write(frame)
+
+            cv2.imshow("frame", frame)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        else:
+            break
+
+    # Release everything if job is finished
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    create_move(0)
---
+++ main.py
@@ -0,0 +1,6 @@
+from move_comparison import compare_positions
+
+benchmark_video = "dance_videos/benchmark_dance.mp4"
+user_video = "dance_videos/right_dance.mp4"  # replace with 0 for webcam
+
+compare_positions(benchmark_video, user_video)
